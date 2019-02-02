0732e67f47a8462974861581721528f863c750c1
pgchui <alex.zhumh@gmail.com>
2019-02-02 16:52:49 -0500

    add Pet Full content
    - Feed btn
    - Refill btn

--- func_webserver/func_webserver.ino
+++ func_webserver/func_webserver.ino
@@ -17,6 +17,8 @@ String output4State = "off";
 const int output5 = 5;
 const int output4 = 4;
 
+int state = 2;
+
 void setup() {
   Serial.begin(115200);
   // Initialize the output variables as outputs
@@ -65,22 +67,17 @@ void loop(){
             client.println();
             
             // turns the GPIOs on and off
-            if (_header.indexOf("GET /5/on") >= 0) {
-              Serial.println("GPIO 5 on");
-              output5State = "on";
-              digitalWrite(output5, HIGH);
-            } else if (_header.indexOf("GET /5/off") >= 0) {
-              Serial.println("GPIO 5 off");
-              output5State = "off";
-              digitalWrite(output5, LOW);
-            } else if (_header.indexOf("GET /4/on") >= 0) {
-              Serial.println("GPIO 4 on");
-              output4State = "on";
-              digitalWrite(output4, HIGH);
-            } else if (_header.indexOf("GET /4/off") >= 0) {
-              Serial.println("GPIO 4 off");
-              output4State = "off";
-              digitalWrite(output4, LOW);
+            if (_header.indexOf("GET /1") >= 0) {
+              state = 1;
+              Serial.println("******Feed once******");
+            } else if (_header.indexOf("GET /0") >= 0) {
+              state = 0;
+              Serial.println("******Feed twice******");
+            } else if (_header.indexOf("GET /u") >= 0) {
+              Serial.println("******Refill needed******");
+            } else if (_header.indexOf("GET /2") >= 0) {
+              Serial.println("******Refilled!******");
+              state = 2;
             }
             
             // Display the HTML web page
@@ -95,25 +92,25 @@ void loop(){
             client.println(".button2 {background-color: #77878A;}</style></head>");
             
             // Web Page Heading
-            client.println("<body><h1>ESP8266 Web Server</h1>");
+            client.println("<body><h1>Pets Full Web Server</h1>");
             
-            // Display current state, and ON/OFF buttons for GPIO 5  
-            client.println("<p>GPIO 5 - State " + output5State + "</p>");
-            // If the output5State is off, it displays the ON button       
-            if (output5State=="off") {
-              client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
-               
-            // Display current state, and ON/OFF buttons for GPIO 4  
-            client.println("<p>GPIO 4 - State " + output4State + "</p>");
-            // If the output4State is off, it displays the ON button       
-            if (output4State=="off") {
-              client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
+            // Display current state  
+            client.println("<p>Num of Feed Left: " + String(state) + "</p>");
+            // If the output5State is off, it displays the ON button
+            switch (state) {
+              case 2:
+                client.println("<p><a href=\"/1\"><button class=\"button\">Feed my baby now!</button></a></p>");     
+                break;
+              case 1:
+                client.println("<p><a href=\"/0\"><button class=\"button\">Feed my baby now!</button></a></p>");
+                break;
+              case 0:
+                client.println("<p><a href=\"/u\"><button class=\"button button2\">Refill it!</button></a></p>");
+                break;
             }
+
+            client.println("<p><a href=\"/2\"><button class=\"button\">I just refilled it!</button></a></p>");
+               
             client.println("</body></html>");
             
             // The HTTP response ends with another blank line
